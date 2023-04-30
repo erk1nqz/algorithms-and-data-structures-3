86a817d8a0cd5a5c0b2efbc82cb1d1dcb8c7ac16
erk1nqz <221326@astanait.edu.kz>
2023-05-01 01:57:37 +0600

    implements methods MyArrayListQueue

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,2 +1,26 @@
-public class MyArrayListQueue {
+public class MyArrayListQueue <E> implements Queue <E> {
+    @Override
+    public void enqueue(E item) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
