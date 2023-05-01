c559c8b94c177273f5a220c6048d85420a5f19b4
erk1nqz <221326@astanait.edu.kz>
2023-05-02 01:08:54 +0600

    supplement all method MyLinkedListStack

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,2 +1,33 @@
-public class MyLinkedListStack {
+public class MyLinkedListStack <E> implements Stack<E> {
+    private MyLinkedList<E> list;
+
+    public MyLinkedListStack() {
+        this.list = new MyLinkedList<>();
+    }
+
+    @Override
+    public void push(E item) {
+        list.add(item);
+
+    }
+
+    @Override
+    public E pop() {
+        return (E) list.remove(0);
+    }
+
+    @Override
+    public E peek() {
+        return (E) list.get(0);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
 }
